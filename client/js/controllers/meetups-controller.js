c5e385e3f358e918822d7504b0a259b3fe093b11
Ravikiran <kiran.perumalla@gmail.com>
2015-05-11 10:29:58 +0530

    Basic Mean app
    Angular App with Express js

---
+++ client/js/controllers/meetups-controller.js
@@ -0,0 +1,4 @@
+
+angular.module("app", []).controller("meetupController", function($scope){
+	$scope.meetupsCount = 10; 
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require( 'express'),
+		app = express();
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/client/views/index.html');
+});
+app.use(express.static('public'));
+app.use('/js', express.static(__dirname + '/client/js/'));
+
+app.listen(3000, function(){
+	console.log('I am listening ....'); 
+});
